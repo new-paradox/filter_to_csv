86856690fe5433de2db15656aa36062dd2c414c5
evgenii ryabenkov <irksunbeam@gmail.com>
2020-09-26 23:07:31 +0800

    changed sys to argparse

--- run_script.py
+++ run_script.py
@@ -1,9 +1,13 @@
-from sys import argv
+import argparse
 import csv_filter
 
-script_name, src = argv
-try:
-    filter_csv = csv_filter.FilterCSV(src)
-    filter_csv.csv_reader()
-except FileNotFoundError as exc:
-    print("Don't worry, the path may be incorrectly entered")
+parser = argparse.ArgumentParser(description='CSV filter')
+parser.add_argument('-p', '--path', type=str, metavar='', help='directory path')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    try:
+        filter_csv = csv_filter.FilterCSV(src=args.path)
+        filter_csv.csv_reader()
+    except FileNotFoundError as exc:
+        print(f"Error: {exc} type {(type(exc))}")
